f6fb0ed629588c149fe03421562d0817cc50cde7
solanoctua <viasolavitae@gmail.com>
2022-03-06 22:22:44 +0300

    angle of the arrow corrected

--- Source/ArrowDirectionDetection.py
+++ Source/ArrowDirectionDetection.py
@@ -5,11 +5,13 @@ pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesserac
 
 cam = cv2.VideoCapture(0)
 color = (255,255,255)
+def distance_between_points(point1, point2):
+    return np.sqrt(np.power(point1[0]-point2[0],2) + np.power(point1[1]-point2[1],2))
 def nothing(x):
     pass
 cv2.namedWindow("CornerTrackbars")
 cv2.createTrackbar("qualityLevel", "CornerTrackbars", 1, 49, nothing)
-cv2.createTrackbar("maxCorners", "CornerTrackbars", 5, 20, nothing)
+cv2.createTrackbar("maxCorners", "CornerTrackbars", 7, 20, nothing)
 cv2.createTrackbar("minDistance", "CornerTrackbars", 10, 15, nothing)
 cv2.createTrackbar("blockSize", "CornerTrackbars", 3, 15, nothing)
 """
@@ -37,10 +39,10 @@ else:
 while ret :
     ret,frame = cam.read()
     frame = cv2.imread("Seeker/TargetImages/arrow1.png")
-    frame_width, frame_height = (480,480)
-    frame = cv2.resize(frame,(frame_width, frame_height))
+    frame_width, frame_height = (640,640)
+    frame = cv2.resize(frame,(frame_width, frame_height ))
     #frame =cv2.flip(frame,-1)
-    center_frame = (frame_width//2, frame_height//2)
+    center_frame = (frame_width//2,frame_height//2)
     blurred = cv2.GaussianBlur(frame,(3,3),0)
     # convert to HSV colorspace 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -53,12 +55,12 @@ while ret :
     cv2.putText(frame,"FPS:{}".format(int(fps)),(15,15),cv2.FONT_HERSHEY_SIMPLEX,.5,(255,255,255),1,cv2.LINE_AA)#Displays fps
     # lines for left,right,up,down boundaries
     #cv2.circle(frame , center_frame, 15,(0,255,0), 1)
-    target_lock_radius = 100
-    #cv2.circle(frame, (frame_width//2, frame_height//2), target_lock_radius, (255,0,0), 1)
-    cv2.line(frame,(int(frame_width/2),0),(int(frame_width/2),int(frame_height)),(255,0,0),1)
-    cv2.line(frame,(0,int(frame_height/2)),(int(frame_width),int(frame_height/2)),(255,0,0),1)
+    target_lock_radius = 75
+    cv2.circle(frame, (frame_width//2, frame_height//2), target_lock_radius, (0,255,0), 1)
+    cv2.line(frame,(int(frame_width/2),0),(int(frame_width/2),int(frame_height)),(0,255,0),1) # vertical line
+    cv2.line(frame,(0,int(frame_height/2)),(frame_width,int(frame_height/2)),(0,255,0),1) # horizontal line
     
-    
+       
     #H,S,V = cv2.split(hsv_frame)
     min_h = cv2.getTrackbarPos("min - H", "ColorTrackbars")
     min_s = cv2.getTrackbarPos("min - S", "ColorTrackbars")
@@ -75,91 +77,79 @@ while ret :
     #contours = max(contours, key = cv2.contourArea)
     
     contours = sorted(contours, key = cv2.contourArea)
-    contours = contours[-1:] # Take the object with the largest area
+    target_contours = contours[-1:] # Take the object with the largest area
     
     """
     contours, hierarchy = cv2.findContours(hsv_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
     """
             
-    for contour in contours:
+    for contour in target_contours:
         if cv2.contourArea(contour) >= 500: # If area is big enough, find its center etc.
             contour = cv2.approxPolyDP(contour, 10, closed=True)
+            
             #print(len(contour))
             if 7 < len(contour) < 20 :
-                #print("sign")
+                print("sign")
                 pass
                 
-            cv2.drawContours(frame, contour, -1, (255,255,0), 15, lineType = cv2.FILLED)
-            
+            cv2.drawContours(frame, contour, -1, (255,150,0), 15, lineType = cv2.FILLED)
+            # Find pointer(sharp point of the arrow) 
+            points = contour.ravel()
+            pointer = (int(points[0]), int(points[1]))
+            cv2.circle(frame, pointer, 10, (0,255,255),-1)
+            # Find center of the contour
             moment = cv2.moments(contour) # To find the center of the contour, we use cv2.moment
             (x_contour, y_contour) = (moment['m10'] / (moment['m00'] + 1e-5), moment['m01'] / (moment['m00'] + 1e-5)) # calculate center of the contour
             center_contour = (int(x_contour), int(y_contour))
-            cv2.circle(frame, center_contour, 5, (255,0,255),2)
-                    
-            (x_contour_circle, y_contour_circle), radius = cv2.minEnclosingCircle(contour)
-            radius = int(radius)
-            center_contour_circle = (int(x_contour_circle), int(y_contour_circle))
-            cv2.circle(frame, center_contour_circle, radius, (255,0,255),2)
-            #Find corners
+            cv2.circle(frame, center_contour, 10, (255,150,0),-1)
+            # Find corners
             mask_arrow = np.ones(frame.shape[:2], dtype="uint8") * 255
-            
             #cv2.drawContours(mask_arrow, contour, -1, (0,0,0), 15, lineType = cv2.FILLED)
+            """
             ConvexHullPoints = cv2.convexHull(contour)
-            #cv2.fillPoly(mask_arrow, ConvexHullPoints, color=(0,0,0))
+            cv2.fillPoly(frame, ConvexHullPoints, color=(255,0,255))
+            """
             cv2.polylines(mask_arrow, [contour], True, (0,255,255), 2)#ConvexHullPoints
             
-            set_qualityLevel = cv2.getTrackbarPos("qualityLevel", "CornerTrackbars")
-            set_maxCorners = cv2.getTrackbarPos("maxCorners", "CornerTrackbars")
-            set_minDistance = cv2.getTrackbarPos("minDistance", "CornerTrackbars")
-            set_blockSize = cv2.getTrackbarPos("blockSize", "CornerTrackbars")
+            # Drawing lines
+            cv2.line(frame, center_contour, pointer,(255,0,255),1)
+            cv2.line(frame, center_frame, center_contour,(0,0,255),1)
+            # Find angle of the arrow
+            atan = math.atan2(pointer[1]-center_contour[1], pointer[0]-center_contour[0])
+            angle_arrow = math.degrees(atan)
+            angle_arrow = int(90+angle_arrow)
+            # Angle of the line connecting center of contour to the center of the frame
+            atan = math.atan2(center_frame[1]-center_contour[1], center_frame[0]-center_contour[0])
+            angle_target = math.degrees(atan)
+            angle_target = int(angle_target - 90)
+            color = (0,0,255)
+            if 0 <= np.abs(angle_arrow) and np.abs(angle_arrow) <= 90:
+                cv2.putText(frame, "FORWARD", (frame_width - 100, 35) , cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 2)
+            if 90 < np.abs(angle_arrow):
+                cv2.putText(frame, "BACKWARD", (frame_width - 100, 35) , cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 2)
+            if angle_arrow < 0:
+                cv2.putText(frame, "LEFT", (frame_width - 100, 55) , cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 2)
+            if angle_arrow > 0:
+                cv2.putText(frame, "RIGHT", (frame_width - 100, 55) , cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 2)
+
+            cv2.putText(frame, "{}*".format(angle_arrow), (frame_width - 100, 15) , cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 2)
+            cv2.putText(frame, "{}*".format(angle_target-angle_arrow), center_frame , cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 2)
+            if distance_between_points(center_contour, center_frame) < target_lock_radius:
+                cv2.circle(blank, center_frame, target_lock_radius,(0,255,0), cv2.FILLED)
+                alpha = 0.4
+                beta = (1.0 - alpha)
+                cv2.addWeighted(blank, alpha, frame, beta, 0.0,frame) # to make rectangle transparent
+
+            kernel = np.ones((5,5), np.uint8)
+            mask_color = cv2.erode(mask_color, kernel, iterations=1)
+            mask_color = cv2.dilate(mask_color, kernel, iterations=1)
+            ret,mask_color = cv2.threshold(np.array(mask_color), 125, 255, cv2.THRESH_BINARY_INV)
+            #print("text: ",pytesseract.image_to_string(mask_color, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')) 
+            #print("text: ",pytesseract.image_to_string(mask_color, config='digits'))
             
-            set_qualityLevel /= 100
-            corners = cv2.goodFeaturesToTrack(mask_arrow, maxCorners = set_maxCorners, qualityLevel = set_qualityLevel ,minDistance = set_minDistance, blockSize = set_blockSize, useHarrisDetector = 1, k = 0.04)
-            
-            if corners is not None:
-                if len(corners) >= 5:
-                    corners = np.int0(corners)
-                    mean_x = 0
-                    mean_y = 0
-                    for i in corners:
-                        x,y = i.ravel()
-                        cv2.circle(frame,(x,y),5,(0,0,255),-1)
-                        mean_x += x
-                        mean_y += y
-                        
-                    x = mean_x // 5
-                    y = mean_y // 5
                     
-                    center = (int(x), int(y))
-                    cv2.circle(frame,(int(center[0]), int(center[1])), 5,(0,255,0), -1)
-                    cv2.circle(mask_arrow,(int(center[0]), int(center[1])), 5,(0,255,0), -1)
-                    cv2.putText(frame, "corners midpoint({},{})".format(center[0],center[1]),(int(x)+15, int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0,255,0), 1)
-                    #print("mid point", center)        
-                    #Drawing lines
-                    cv2.line(frame, center_contour, center,(255,0,255),1)
-                    cv2.line(frame, center_frame, center_contour,(0,0,255),1)
-                    cv2.line(frame, center_frame, center,(0,0,255),1)
-                    #Angle of the arrow
-                    atan = math.atan2(center[1]-center_contour[1], center[0]-center_contour[0])
-                    angle_arrow = math.degrees(atan)
-                    angle_arrow = int(90-angle_arrow)
-                    #Angle of the line connecting center of contour to the center of the frame
-                    atan = math.atan2(center_frame[1]-center_contour[1], center_frame[0]-center_contour[0])
-                    angle_target = math.degrees(atan)
-                    angle_target = int(90-angle_target)
-                    cv2.putText(frame, "{}*".format(angle_arrow), center_contour , cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 1)
-                    cv2.putText(frame, "{}*".format(angle_target-angle_arrow), center_frame , cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 2)
-                    #print ("angle = ", angle)
-                    kernel = np.ones((5,5), np.uint8)
-                    mask_color = cv2.erode(mask_color, kernel, iterations=1)
-                    mask_color = cv2.dilate(mask_color, kernel, iterations=1)
-                    ret,mask_color = cv2.threshold(np.array(mask_color), 125, 255, cv2.THRESH_BINARY_INV)
-                    #print("text: ",pytesseract.image_to_string(mask_color, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')) 
-                    #print("text: ",pytesseract.image_to_string(mask_color, config='digits'))
-                    
-            cv2.imshow("mask_arrow",mask_arrow)
-    
+    cv2.imshow("mask_arrow",mask_arrow)
     cv2.imshow("realTimeCamera",frame)    
     cv2.imshow("mask_color",mask_color)
     
