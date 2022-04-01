4f3258e6b7bc4836b46aa87b54c090008cda6cd0
solanoctua <viasolavitae@gmail.com>
2022-04-01 12:50:02 +0300

    try except for camera failure

--- Source/VisualOdometry_Monocular.py
+++ Source/VisualOdometry_Monocular.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2 # cv2.__version__ = '4.0.0'
+import cv2, time # cv2.__version__ = '4.0.0'
 
 class VisualOdometryMono():
     def __init__(self, *args, **kwargs):
+        self.visuals = 1
+        self.record = 0
         # CAMERA PARAMETERS
         self.cam = cv2.VideoCapture(0)
         self.frame_width, self.frame_height = (340,340)  # 1280,720
@@ -13,7 +15,7 @@ class VisualOdometryMono():
         self.DistortionCoefficients = np.array( [ 1.3876171595109568e-01, -5.0495233579131149e-01, -1.4364550355797534e-03, 3.0938437583063767e-03, 1.3034844698951493e+00 ])
         self.FeatureDetector = cv2.ORB_create(nfeatures= 500) # https://docs.opencv.org/4.0.0/db/d95/classcv_1_1ORB.html
         # Flann parameters for ORB
-        indexparameters= dict(algorithm = 6,
+        indexparameters= dict(algorithm = 6, 
                               table_number = 12,# 6, # 12
                               key_size = 20,# 12,     # 20
                               multi_probe_level = 2)#1) #2
@@ -24,53 +26,81 @@ class VisualOdometryMono():
         self.Frames = []
 
         np.set_printoptions(suppress= True)
-    def computeKeyandDes(frame):
+
+    def computeKeyandDes(self, frame):
+        print("computing keypoints and descriptors..")
         """
         features = cv2.goodFeaturesToTrack(np.mean(frame, axis=2).astype(np.uint8), mask = None, **goodFeatureParams)
         keypoints = [cv2.KeyPoint(x = feature[0][0], y = feature[0][1], _size = 20) for feature in features ]  # size: diameter of the meaningful keypoint neighborhood
         keypoints, descriptors = self.FeatureDetector.compute(frame, keypoints)
         """
         keypoints, descriptors = self.FeatureDetector.detectAndCompute(frame, None)
+        print("keypoints = ",keypoints)
+        print("descriptors = ",descriptors)
         return keypoints, descriptors
 
-    def findMatches(self, frame_number):
-        old_keypoints, old_descriptors = computeKeyandDes(Frames[frame_number - 1])
-        new_keypoints, new_descriptors = computeKeyandDes(Frames[frame_number])
+    def findMatches(self, frame_number, frame):
+        print("finding matches..")
+        old_keypoints, old_descriptors = self.computeKeyandDes(self.Frames[frame_number - 1])
+        new_keypoints, new_descriptors = self.computeKeyandDes(self.Frames[frame_number])
 
         goodMatches = []
-        matches = flann.knnMatch(old_descriptors, new_descriptors, k=2)
-        for m,n in matches:
-            if m.distance < ratioThresh * n.distance:  # Lowe's ratio test
+        try: # if there is no neighboor to pick two of them, this function will raise an error and we will catch it.
+            matches = self.Matcher.knnMatch(old_descriptors, new_descriptors, k=2)
+        except cv2.error as e:
+            print(e)
+            if ((old_descriptors is None) or (new_descriptors is None)):
+                print("ERROR: No Descriptor!")
+
+        print("matches: ",matches)
+        for pair in matches:
+            try:
+                m, n = pair
+            except:
+                continue
+            if m.distance < self.GoodMatchesRatio * n.distance:  # Lowe's ratio test
                 goodMatches.append(m)
                 """
                 srcPoints.append(old_keypoints[m.queryIdx].pt) # points in the new frame
                 dstPoints.append(new_keypoints[m.trainIdx].pt) # points in the old frame
-                """
-        srcPoints = np.float32[old_keypoints[m.queryIdx].pt for m in goodMatches] # points in the old frame 
-        dstPoints = np.float32[new_keypoints[m.trainIdx].pt for m in goodMatches] # points in the new frame
-        if(visuals == 1):
-            for (point_old, point_new) in goodMatches:
-                x1,y1 = point_old.pt
-                x2,y2 = point_new.pt
+                """ 
+        print("len(goodMatches): ",len(goodMatches))       
+        srcPoints = np.float32([old_keypoints[m.queryIdx].pt for m in goodMatches]).reshape(-1, 1, 2) # points in the old frame 
+        dstPoints = np.float32([new_keypoints[m.queryIdx].pt for m in goodMatches]).reshape(-1, 1, 2) # points in the new frame
+        
+        if(self.visuals == 1):
+            """
+            for m in goodMatches:
+                x1,y1 = srcPoints[m.queryIdx].pt
+                x2,y2 = dstPoints[m.queryIdx].pt
                 cv2.line(frame, (round(x1),round(y1)), (round(x2),round(y2)), (255,0,0),2)
                 cv2.circle(frame, (int(x1), int(y1)), 5, (0,0,255), 1)
+            """
+            draw_params = dict(matchColor = (255,0,255), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = None, # draw only inliers
+                   flags = 2)
+            result = cv2.drawMatches(self.Frames[frame_number], new_keypoints, self.Frames[frame_number-1], old_keypoints, goodMatches,None,**draw_params)
         return srcPoints, dstPoints
-
+    @staticmethod
     def calculateTransformationMatrix(rotationMatrix, translationVector):
+        print("calculating transformation matrix..")
         translationMatrix = np.eye(4, dtype= np.float64)
         translationMatrix[:3,:3] = rotationMatrix
         translationMatrix[:3,3] = translationVector
         return translationMatrix
 
     def decomposeEssentialMatrix(self, EssentialMatrix, point_old, point_new):
+        print("decomposing essential matrix")
         R1, R2, t = cv2.decomposeEssentialMat(EssentialMatrix)# https://docs.opencv.org/4.0.0/d9/d0c/group__calib3d.html#ga54a2f5b3f8aeaf6c76d4a31dece85d5d
-        T1 = calculateTransformationMatrix(R1, t)
-        T2 = calculateTransformationMatrix(R2, t)
-        T3 = calculateTransformationMatrix(R1, -t)
-        T4 = calculateTransformationMatrix(R2, -t)
+        print("R1 = {}\n R2 = {}\n  t = {}".format(R1, R2, t))
+        T1 = self.calculateTransformationMatrix(R1, t)
+        T2 = self.calculateTransformationMatrix(R2, t)
+        T3 = self.calculateTransformationMatrix(R1, -t)
+        T4 = self.calculateTransformationMatrix(R2, -t)
 
         T = [T1, T2, T3, T4]
-
+        print("T = [T1, T2, T3, T4] = {} ".format(T))
         # Make cameraMatrix homogeneous
         cameraMatrix = np.concatenate((self.CameraMatrix, np.zeros((3,1))), axis= 1) # make last row of the cameraMatrix 0 0 0
         possiblePoses = [cameraMatrix @ T1, cameraMatrix @ T2, cameraMatrix @ T3, cameraMatrix @ T4]
@@ -100,60 +130,64 @@ class VisualOdometryMono():
 
     def estimatePose(self, oldPoints, newPoints):
         EssentialMatrix, mask = cv2.findEssentialMat(oldPoints, newPoints, self.CameraMatrix)
-        R, t = decomposeEssentialMatrix(EssentialMatrix)
-        return calculateTransformationMatrix(R, t)
+        R, t = self.decomposeEssentialMatrix(EssentialMatrix, oldPoints, newPoints)
+        return self.calculateTransformationMatrix(R, t)
 
-    def main():
-        vo = VisualOdometryMono()
-        
-        prev_frame_time = 0
-        new_frame_time = 0
-        if cam.isOpened():
-            ret,frame = cam.read()
-            frame_number = 1
-            vo.Frames.append(frame)
-            output = cv2.VideoWriter("output.avi", cv2.VideoWriter_fourcc(*'XVID'), 2.0, (frame_width, frame_height)) # 'M','J','P','G' #https://docs.opencv.org/3.4/dd/d9e/classcv_1_1VideoWriter.html
-        else: 
+def main():
+    vo = VisualOdometryMono()
+    
+    prev_frame_time = 0
+    new_frame_time = 0
+    print("ola0")
+    if vo.cam.isOpened():
+        print("ola1")
+        ret,frame = vo.cam.read()
+        frame_number = 0
+        vo.Frames.append(frame)
+        output = cv2.VideoWriter("output.avi", cv2.VideoWriter_fourcc(*'XVID'), 15.0, (vo.frame_width, vo.frame_height)) # 'M','J','P','G' #https://docs.opencv.org/3.4/dd/d9e/classcv_1_1VideoWriter.html
+    else: 
         ret = False
-        while ret :
-            ret,frame = cam.read()
-            vo.Frames.append(frame)
-            frame_number += 1
-            frame = cv2.resize(frame,(frame_width, frame_height ))
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # apply grayscale
-            #frame =cv2.flip(frame,-1)
+    while ret :
+        ret,frame = vo.cam.read()
+        frame = cv2.imread("Seeker/TargetImages/arrow1.png")
+        vo.Frames.append(frame)
+        frame_number += 1
+        frame = cv2.resize(frame,(vo.frame_width, vo.frame_height ))
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # apply grayscale
+        #frame =cv2.flip(frame,-1)
 
-            #Calculate FPS
-            new_frame_time = time.time()
-            fps = 1/(new_frame_time-prev_frame_time)
-            prev_frame_time = new_frame_time
-            
-            points2D_old, points2D_new = vo.findMatches(frame_number)
-            transformationMatrix = vo.estimatePose(points2D_old, points2D_new)
-            
-            if(visuals == 1):
-                cv2.putText(frame,"FPS:{}".format(int(fps)),(15,15),cv2.FONT_HERSHEY_SIMPLEX,.5,(0,0,255),1,cv2.LINE_AA)# Displays fps
-                cv2.imshow("realTimeCamera", frame)
-            if(record == 1):
-                output.write(frame)   
-            key=cv2.waitKey(1)
-            if key==27:
-                break
-        cv2.destroyAllWindows()
-        cam.release()
-        output.release()
-    # FEATURE DETECTION
-
-
-    # FEATURE MATCHING/TRACKING
+        #Calculate FPS
+        new_frame_time = time.time()
+        fps = 1/(new_frame_time-prev_frame_time)
+        prev_frame_time = new_frame_time
+        
+        points2D_old, points2D_new = vo.findMatches(frame_number, gray)
+        transformationMatrix = vo.estimatePose(points2D_old, points2D_new)
+        print(transformationMatrix)
+        if(vo.visuals == 1):
+            cv2.putText(frame,"FPS:{}".format(int(fps)),(15,15),cv2.FONT_HERSHEY_SIMPLEX,.5,(0,0,255),1,cv2.LINE_AA)# Displays fps
+            cv2.imshow("realTimeCamera", frame)
+        if(vo.record == 1):
+            output.write(frame)   
+        key=cv2.waitKey(1)
+        if key==27:
+            break
+    cv2.destroyAllWindows()
+    vo.cam.release()
+    output.release()
+# FEATURE DETECTION
 
 
-    # POSE ESTIMATION
+# FEATURE MATCHING/TRACKING
 
 
-    # BUNDLE ADJUSTMENT
+# POSE ESTIMATION
 
 
-    #
+# BUNDLE ADJUSTMENT
 
 
+#
+
+
+main()
