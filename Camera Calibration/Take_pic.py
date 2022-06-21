14664265d2077ab67a85c9f50af0548d00990b55
solanoctua <79167361+solanoctua@users.noreply.github.com>
2022-06-21 14:16:40 +0300

    Update Take_pic.py

--- Camera Calibration/Take_pic.py
+++ Camera Calibration/Take_pic.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 
 
-path_of_images = "C:/Users/asus/Desktop/Samurai-Seeker/FisheyeCalibration"
+path_of_images = "./"
 cap = cv2.VideoCapture(0)
 frame_width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   
 frame_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  
@@ -14,6 +14,7 @@ else:
     ret = False
 while ret:
     ret , frame = cap.read()
+    clone = frame.copy()
     cv2.putText(frame,"press 'esc' to quit",(15,15), cv2.FONT_HERSHEY_SIMPLEX, .6,(0,0,255),1,cv2.LINE_AA) #displays some text
     cv2.putText(frame,"press 's' to save the image",(15,35), cv2.FONT_HERSHEY_SIMPLEX, .6,(255,0,0),1,cv2.LINE_AA) #displays some text 
     cv2.imshow("camera",frame)
@@ -21,7 +22,7 @@ while ret:
     key = cv2.waitKey(1)
     if key == ord("+"):
         print("Saving image to ",path_of_images)
-        cv2.imwrite("{}{}.jpg".format(path_of_images+"/",count), frame)
+        cv2.imwrite("{}{}.jpg".format(path_of_images+"/",count), clone)
         count += 1
     if key == 27:   #press esc to quit
         break
